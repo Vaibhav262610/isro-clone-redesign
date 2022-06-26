83fa7ca8b8c081b8c5cfbeb49a87c92294cf3d39
Vaibhav262610 <branda56235@gmail.com>
2022-06-26 05:09:09 -0700

    add

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,16 @@
 import Head from "next/head";
-import Image from "next/image";
+ 
 import AboutNav from "../components/AboutNav";
 import AboutMain from "../components/AboutMain";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import Navbar from "../components/Navbar";
-import styles from "../styles/Home.module.css";
-
+ 
 export default function Home() {
   return (
     <>
       <Head>
         <title>ISRO-REDESIGN</title>
+        <link rel="shortcut icon" href="/img/astronaut_icon.png" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link
